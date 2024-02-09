3624aae071022c63974002a806398ca360391dbf
aemr-LL7 <a.marquez2020@correo.unimet.edu.ve>
2024-02-08 22:47:10 -0400

    First commit

---
+++ dist/helloTest.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hello brav");
+const a = 2;
+const b = 5;
+let sum = a + b;
+console.log("Suma: " + sum);
+//# sourceMappingURL=helloTest.js.map
---
+++ src/helloTest.ts
@@ -0,0 +1,6 @@
+console.log("Hello brav");
+const a: number= 2;
+const b: number= 5;
+
+let sum: number = a + b ;
+console.log("Suma: " + sum);
